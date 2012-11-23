ecc9f4ea3ac5850d1e3b5cf6d221f5a4e5a079f3
Filip Åstrand <Filip.astrand@elev.it-gymnasiet.se>
2012-11-23 10:24:01 +0100

    added player class

--- game.rb
+++ game.rb
@@ -6,9 +6,19 @@ class Game < Chingu::Window
 	def initialize
 		super
 		self.input = {esc: :exit}
+		@player = Player.create
 	end
 
 
 end
 
+class player < Chingu::GameObject
+	
+	# Meta-constructor
+	def setup
+		@x, @y = 350, 400
+		@image = Gosu::Image["spaceship.png"]
+	end
+
+end
 Game.new.show
