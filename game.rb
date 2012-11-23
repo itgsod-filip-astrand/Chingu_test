9715337e958de8c09260b6a62c72a4e316991baf
Filip Åstrand <Filip.astrand@elev.it-gymnasiet.se>
2012-11-23 10:31:25 +0100

    Added player movement

--- game.rb
+++ game.rb
@@ -18,6 +18,24 @@ class player < Chingu::GameObject
 	def setup
 		@x, @y = 350, 400
 		@image = Gosu::Image["spaceship.png"]
+		self.input = {
+			holding_left: :left,
+			holding_right: :right,
+			holding_up: :up,
+			holding_down: :down
+		}
+	end
+	def left
+		@x -=1
+	end
+	def right
+		@x +=1
+	end
+	def up
+		@y -=2
+	end
+	def down
+		@y +=2
 	end
 
 end
